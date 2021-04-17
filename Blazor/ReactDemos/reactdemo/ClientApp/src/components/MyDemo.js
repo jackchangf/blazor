c539582d89a40591275845d49f1f46583ee768f9
Jack Chan G F <changuofeng@gmail.com>
2021-04-17 17:19:44 +0800

    blazor pwa react demo

---
+++ Blazor/ReactDemos/reactdemo/ClientApp/src/components/MyDemo.js
@@ -0,0 +1,16 @@
+﻿import React, { Component } from 'react'
+
+export class MyDemo extends Component {
+
+    constructor(props) {
+        super(props);
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>Hello World Demo</h1>
+            </div>
+            );
+    };
+};
